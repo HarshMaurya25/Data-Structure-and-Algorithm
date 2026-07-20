cabe43bb88cb7ccf3f6fedeb9358702c9baabcff
Harsh <mauryaharsh2505@gmail.com>
2026-07-20 22:57:39 +0530

    Lower bound

--- Binary Seach Tree/Upperbound.py
+++ Binary Seach Tree/Upperbound.py
@@ -11,6 +11,20 @@ def upperBound(arr, target):
             high = mid - 1
 
     return low if low < len(arr) else -1
+
+def lowerBound(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = low + (high - low) // 2
+
+        if arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return low if low < len(arr) else -1
     
 arr = [1, 2, 2, 2, 2, 2, 2]
-print(upperBound(arr , 2))
+print(lowerBound(arr , 2))
