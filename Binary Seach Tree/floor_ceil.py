29ba136825d1ba9302d3bb72ef67694586fee897
Harsh <mauryaharsh2505@gmail.com>
2026-07-20 22:49:37 +0530

    Floor and ceil

---
+++ Binary Seach Tree/floor_ceil.py
@@ -0,0 +1,37 @@
+def floor(arr, target):
+    low = 0
+    high = len(arr) - 1
+    ans = -1
+
+    while low <= high:
+        mid = low + (high - low) // 2
+
+        if arr[mid] <= target:
+            ans = mid         
+            low = mid + 1      
+        else:
+            high = mid - 1
+
+    return ans
+
+def ceil(arr , target):
+    low = 0
+    high = len(arr) - 1
+    ans = -1
+
+    while low <= high:
+        mid = low + (high - low) // 2
+
+        if arr[mid] < target:    
+            low = mid + 1      
+        else:
+            high = mid - 1
+            ans = mid     
+
+    return ans
+
+
+arr = [1, 2, 3, 6, 7, 22, 23, 26, 45, 465, 999]
+
+idx = ceil(arr, 9)
+print(idx) 
