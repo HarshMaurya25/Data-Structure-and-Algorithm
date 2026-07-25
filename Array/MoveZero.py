ab506c5b404953465c86629a581bd7ebdeff9078
Harsh <mauryaharsh2505@gmail.com>
2026-07-25 20:08:11 +0530

    283. Move Zeroes [Easy]

---
+++ Array/MoveZero.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        insertPos = 0
+
+        for num in nums:
+            if num != 0:
+                nums[insertPos] = num
+                insertPos += 1
+
+        while insertPos < len(nums):
+            nums[insertPos] = 0
+            insertPos += 1
