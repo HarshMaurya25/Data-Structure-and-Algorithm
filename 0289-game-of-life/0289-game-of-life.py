5b6b03ed85cfe30363e3b6c3300e177f2ccc1bb4
Harsh Maurya <mauryaharsh2505@gmail.com>
2026-09-04 22:24:46 +0530

    Time: 0 ms (100%), Space: 19.5 MB (15.98%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -1,36 +1,44 @@
 class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
-        rows = len(board)
-        cols = len(board[0])
+        direction = [-1, 1 , 0]
+        coordinate = []
+        for d in direction:
+            for d2 in direction:
+                coordinate.append([d , d2])
+        
+        coordinate.pop()
 
-        directions = [
-            (-1, -1), (-1, 0), (-1, 1),
-            (0, -1),           (0, 1),
-            (1, -1),  (1, 0),  (1, 1)
-        ]
-
-        for y in range(rows):
-            for x in range(cols):
+        def legal(nx, ny):
+            if nx < 0 or ny < 0:
+                return False
+            if nx >= len(board):
+                return False
+            if ny >= len(board[0]):
+                return False
+            
+            return True
+        
+        for idxr,r in enumerate(board):
+            for idxc, c in enumerate(r):
                 count = 0
+                for d in coordinate:
+                    nx = d[0] + idxr 
+                    ny =  d[1] + idxc
 
-                for dy, dx in directions:
-                    ny = y + dy
-                    nx = x + dx
-
-                    if 0 <= ny < rows and 0 <= nx < cols:
-                        if board[ny][nx] in (1, 3):
-                            count += 1
-
-                if board[y][x] == 1:
-                    if count < 2 or count > 3:
-                        board[y][x] = 3
-                else:
+                    if legal(nx,ny) and board[nx][ny] in [1, 3]:
+                        count += 1
+                if c == 0:
                     if count == 3:
-                        board[y][x] = 2
-                        
-        for y in range(rows):
-            for x in range(cols):
-                if board[y][x] == 2:
-                    board[y][x] = 1
-                elif board[y][x] == 3:
-                    board[y][x] = 0
+                        board[idxr][idxc] = 2
+                else:
+                    if count < 2 or count > 3:
+                        board[idxr][idxc] = 3
+        
+        for idxr,r in enumerate(board):
+            for idxc, c in enumerate(r):
+                if c == 3:
+                    board[idxr][idxc] = 0
+                elif c == 2:
+                    board[idxr][idxc] = 1
+
+        
