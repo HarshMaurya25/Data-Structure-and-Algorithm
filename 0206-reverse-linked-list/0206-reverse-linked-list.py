8a31baf858724aeb0b3dd97f42a7e5aa53ddd177
Harsh Maurya <mauryaharsh2505@gmail.com>
2026-09-09 20:08:10 +0530

    Time: 0 ms (100%), Space: 20.6 MB (7.34%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -1,20 +1,12 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return head
-        temp = head.next
-        temp2 = head
-        temp2.next = None
+        prev = None
+        curr = head
 
-        while temp:
-            x = temp.next
-            temp.next = temp2
-            temp2 = temp
-            temp = x
-        
-        return temp2
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        return prev
